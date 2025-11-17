b7438b7d4314281494e98e79c8ab4205b1cb2fe7
althafabdulraheem <althafbinraheem786@gmail.com>
2025-11-17 22:03:51 +0400

    made responsive landing page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,9 +2,9 @@
 import { motion } from "framer-motion";
 export default function Hero() {
   return (
-    <div className="w-full flex p-2 ">
+    <div className="w-full p-2 flex flex-col-reverse flex-col sm:flex-row md:flex-row">
       <motion.div
-        className="w-3/5 flex flex-col justify-center items-center h-[80vh] md:w-full"
+        className="w-full sm:w-3/5 md:w-3/5 flex flex-col md:justify-center md:items-center h-[60vh] sm:h-[80vh] md:h-[80vh] "
         initial={{ opacity: 0, x: -80 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1.5 }}
@@ -21,19 +21,19 @@ export default function Hero() {
           </p>
         </div>
       </motion.div>
-      <div className="w-2/5 flex justify-center items-center h-[80vh]">
+      <div className=" w-full sm:w-2/5 flex md:justify-center md:items-center h-[80vh]">
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 2 }}
-          className="mt-4 h-[400px] w-[400px] bg-cyan-800 absolute"
+          className="hidden sm:block sm:mt-4 h-[400px] w-[400px] bg-cyan-800 absolute"
           style={{ transform: "rotate3d(2,2,2,69deg)" }}
         ></motion.div>
         <motion.img
           initial={{ opacity: 0, x: 80 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1.5 }}
-          className="rounded-[50%] h-[300px] w-[300px] grayscale-[1] hover:grayscale-[0]"
+          className="rounded-[50%] h-full md:h-[300px] sm:h-[300px] w-full md:w-[300px] sm:w-[300px] grayscale-[1] hover:grayscale-[0]"
           src="https://avatars.githubusercontent.com/u/109522801?v=4"
           alt="hero image"
         />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between bg-black p-4 text-white w-full">
       <h1 className="font-bold">ATFAR</h1>
-      <div className="flex justify-between w-[30%]">
+      <div className="hidden sm:block flex justify-between w-[30%]">
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/projects">Projects</Link>
