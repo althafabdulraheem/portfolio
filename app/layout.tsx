98fff41b395d073cf5fc8cb124ab971f82a7550c
althafabdulraheem <althafbinraheem786@gmail.com>
2025-12-30 22:38:23 +0400

    fixed design issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning className='light'>
-      <body suppressHydrationWarning className={`${poppins.variable}`}>
+      <body suppressHydrationWarning className={`${poppins.variable}  dark:bg-zinc-950`}>
         {children}
         <DarkModeToggle />
       </body>
