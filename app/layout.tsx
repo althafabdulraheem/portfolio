f3f17c4358008dd197c1d3d5f72ea748058e1c08
althafabdulraheem <althafbinraheem786@gmail.com>
2026-01-05 23:04:43 +0400

    added seo tags --og tags & google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Poppins } from 'next/font/google'
 import DarkModeToggle from '@/components/DarkModeToggle';
 import type { Metadata } from 'next'
+import { GoogleTagManager } from '@next/third-parties/google'
 const poppins = Poppins({
   subsets: ['latin'],
   display: 'swap',
@@ -25,14 +26,50 @@ export const metadata: Metadata = {
     shortcut: '/favicon.ico',
     apple: '/favicon.ico',
 
-  }
+  },
+  metadataBase: new URL('https://atfar.netlify.app/'),
+
+  alternates: {
+    canonical: 'https://atfar.netlify.app/',
+  },
+
+  openGraph: {
+    title: 'Althaf Abdul Raheem | Full Stack Developer',
+    description:
+      'Explore projects and experience of Full Stack Developer Althaf Abdul Raheem.',
+    url: 'https://atfar.netlify.app/',
+    siteName: 'Althaf Portfolio',
+    images: [
+      {
+        url: '/og-img.png',
+        width: 1200,
+        height: 630,
+        alt: 'Althaf Abdul Raheem Portfolio',
+      },
+    ],
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    images: ['/og-img.png'],
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
 }
+
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning className='light'>
       <body suppressHydrationWarning className={`${poppins.variable}  dark:bg-zinc-950`}>
         {children}
         <DarkModeToggle />
+        <GoogleTagManager gtmId="G-60C7DPWKC9" />
       </body>
     </html>
   )
